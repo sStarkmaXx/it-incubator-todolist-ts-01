80d102d29d2045c3adf7b1d5abd91efc3c9ceefa
sStarkmaXx <60282152+sStarkmaXx@users.noreply.github.com>
2021-06-16 00:16:30 +0700

    todolist reducer

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,58 @@
+import { v1 } from 'uuid';
+import { FilterType, TodolistType } from '../App';
+
+type RemoveTodoListAT = {
+  type: 'REMOVE-TODOLIST';
+  todoListID: string;
+};
+
+type AddTodoListAT = {
+  type: 'ADD-TODOLIST';
+  title: string;
+};
+
+type ChangeTodoListFilterAT = {
+  type: 'CHANGE-TODOLIST-FILTER';
+  filter: FilterType;
+  todoListID: string;
+};
+
+type ChangeTodoListTitleAT = {
+  type: 'CHANGE-TODOLIST-TITLE';
+  todoListID: string;
+  newTitle: string;
+};
+
+export type ActionType =
+  | RemoveTodoListAT
+  | AddTodoListAT
+  | ChangeTodoListFilterAT
+  | ChangeTodoListTitleAT;
+
+export const todoListsReducer = (
+  todoLists: Array<TodolistType>,
+  action: ActionType
+) => {
+  switch (action.type) {
+    case 'REMOVE-TODOLIST':
+      return todoLists.filter((tl) => tl.id !== action.todoListID);
+    case 'ADD-TODOLIST':
+      const newTodoListId = v1();
+      const newTodoList: TodolistType = {
+        id: newTodoListId,
+        title: action.title,
+        filter: 'all',
+      };
+      return [newTodoList, ...todoLists];
+    case 'CHANGE-TODOLIST-FILTER':
+      return todoLists.map((tl) =>
+        tl.id === action.todoListID ? { ...tl, filter: action.filter } : tl
+      );
+    case 'CHANGE-TODOLIST-TITLE':
+      return todoLists.map((tdl) =>
+        tdl.id === action.todoListID ? { ...tdl, title: action.newTitle } : tdl
+      );
+    default:
+      return todoLists;
+  }
+};
